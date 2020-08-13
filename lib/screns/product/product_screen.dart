31b394657c9185f45dfa34c30e8601e10f462dc5
VINICIUS VICCO PETRARCA DE ASSIS MIRANDA <vinicius.miranda@tre-to.jus.br>
2020-08-13 14:52:54 -0300

    Telas de produto finalizadas + regra de login aplicada.

--- lib/screns/product/product_screen.dart
+++ lib/screns/product/product_screen.dart
@@ -1,6 +1,10 @@
 import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 import 'package:michellemirandastore/models/product.dart';
+import 'package:michellemirandastore/models/user_manager.dart';
+import 'package:provider/provider.dart';
+
+import 'components/size_widget.dart';
 
 class ProductScreen extends StatelessWidget {
 
@@ -11,69 +15,127 @@ class ProductScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final primaryColor = Theme.of(context).primaryColor;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(product.name),
-        centerTitle: true,
-      ),
-      backgroundColor: Colors.white,
-      body: ListView(
-        children: <Widget>[
-          AspectRatio(
-            aspectRatio: 1,
-            child: Carousel(
-              images: product.images.map((url) => NetworkImage(url)).toList(),
-              dotSize: 4,
-              dotSpacing: 15,
-              dotBgColor: primaryColor,
-              dotColor: Colors.white,
-              autoplay: false, // AutoPlay
+    return ChangeNotifierProvider.value(
+      // Create é pra criar um objeto.
+      // .Value é para fornecer um objeto já existente.
+      value: product,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(product.name),
+          centerTitle: true,
+        ),
+        backgroundColor: Colors.white,
+        body: ListView(
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 1,
+              child: Carousel(
+                images: product.images.map((url) => NetworkImage(url)).toList(),
+                dotSize: 4,
+                dotSpacing: 15,
+                dotBgColor: primaryColor,
+                dotColor: Colors.white,
+                autoplay: false, // AutoPlay
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  product.name,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 8),
-                  child: Text(
-                    'A partir de',
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Text(
+                    product.name,
                     style: TextStyle(
-                      color: Colors.grey[600],
-                      fontSize: 13,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
                     ),
                   ),
-                ),
-                Text(
-                  'R\$ 19.99',
-                  style: TextStyle(
-                    fontSize: 22.0,
-                    fontWeight: FontWeight.bold,
-                    color: primaryColor,
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8),
+                    child: Text(
+                      'A partir de',
+                      style: TextStyle(
+                        color: Colors.grey[600],
+                        fontSize: 13,
+                      ),
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 16,bottom: 8),
-                  child: Text(
-                    'Descrição',
+                  Text(
+                    'R\$ 19.99',
                     style: TextStyle(
+                      fontSize: 22.0,
                       fontWeight: FontWeight.bold,
-                      fontSize: 16,
+                      color: primaryColor,
                     ),
                   ),
-                ),
-              ],
-            ),
-          )
-        ],
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16,bottom: 8),
+                    child: Text(
+                      'Descrição',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    product.description,
+                    style: TextStyle(
+                    fontSize: 16,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16,bottom: 8),
+                    child: Text(
+                      'Tamanhos',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                  Wrap(
+                    spacing: 8,
+                    runSpacing: 8,
+                    children: product.sizes.map((s){
+                      return SizeWidget(size: s);
+                    }).toList(),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  if(product.hasStock)
+                  Consumer2<UserManager, Product>(
+                    builder: (_,userManager, product, __){
+                      return SizedBox(
+                        height: 44,
+                        child: RaisedButton(
+                          onPressed: product.selectedSize != null ? (){
+                              if(userManager.isLoggedIn){
+                                //TODO adicionar ao carrinho
+                              } else {
+                                Navigator.of(context).pushNamed('/login');
+                              }
+                          }: null,
+                          color: primaryColor,
+                          textColor: Colors.white,
+                          child: Text(
+                            userManager.isLoggedIn
+                                ? 'Adicionar ao Carrinho'
+                                : 'Entre para adicionar produtos ao Carrinho',
+                            style: TextStyle(
+                              fontSize: 16
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
