ae4d13e9d7a35397e223febeac5976b4c0dc1253
VINICIUS VICCO PETRARCA DE ASSIS MIRANDA <vinicius.miranda@tre-to.jus.br>
2020-09-29 11:50:11 -0300

    Preço visto dinâmicamente, alterações na tela de edição.

--- lib/screns/edit_product/components/images_form.dart
+++ lib/screns/edit_product/components/images_form.dart
@@ -23,66 +23,83 @@ class ImagesForm extends StatelessWidget {
           return 'Insira ao menos uma Imagem';
         } else return null;
       },
-
+      autovalidate: true,
       builder: (state){
         void onImageSelected(File file){
           state.value.add(file);
           state.didChange(state.value);
           Navigator.of(context).pop();
         }
-        return AspectRatio(
-          aspectRatio: 1,
-          child: Carousel(
-            images: state.value.map<Widget>((image){
-              return Stack(
-                fit: StackFit.expand,
-                children: [
-                  if(image is String)
-                    Image.network(image, fit: BoxFit.cover,)
-                  else
-                    Image.file(image as File, fit: BoxFit.cover,),
-                  Align(
-                    alignment: Alignment.topRight,
-                    child: IconButton(
-                      icon: Icon(Icons.remove),
-                      color: Colors.red,
-                      onPressed: (){
-                        if(state.value.length==1){
-                          print("Não pode remover a única imagem");
-                          return;
-                        }
-                        else{
+
+        return Column(
+          children:[ AspectRatio(
+            aspectRatio: 1,
+            child: Carousel(
+              images: state.value.map<Widget>((image){
+                return Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    if(image is String)
+                      Image.network(image, fit: BoxFit.cover,)
+                    else
+                      Image.file(image as File, fit: BoxFit.cover,),
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: IconButton(
+                        icon: Icon(Icons.remove),
+                        color: Colors.red,
+                        onPressed: (){
+//                        if(state.value.length==1){
+//                          print("Não pode remover a única imagem");
+//                          return;
+//                        }
+//                        else{
+//                          state.value.remove(image);
+//                          state.didChange(state.value);
+//                        }
                           state.value.remove(image);
                           state.didChange(state.value);
-                        }
-
-                      },
-                    ),
-                  )
-                ],
-              );
-            }).toList()..add(
-              Material(
-                color: Colors.grey[100],
-                child: IconButton(
-                  color: Theme.of(context).primaryColor,
-                  iconSize: 50,
-                  icon: Icon(Icons.add_a_photo),
-                  onPressed: (){
-                    if(Platform.isAndroid)
-                    showModalBottomSheet(context: context, builder: (_) => ImageSourceSheet(onImageSelected: onImageSelected));
-                    else
-                      showCupertinoModalPopup(context: null, builder: (_) => ImageSourceSheet(onImageSelected: onImageSelected));
-                  },
-                ),
-              )
+                        },
+                      ),
+                    )
+                  ],
+                );
+              }).toList()..add(
+                Material(
+                  color: Colors.grey[100],
+                  child: IconButton(
+                    color: Theme.of(context).primaryColor,
+                    iconSize: 50,
+                    icon: Icon(Icons.add_a_photo),
+                    onPressed: (){
+                      if(Platform.isAndroid)
+                      showModalBottomSheet(context: context, builder: (_) => ImageSourceSheet(onImageSelected: onImageSelected));
+                      else
+                        showCupertinoModalPopup(context: null, builder: (_) => ImageSourceSheet(onImageSelected: onImageSelected));
+                    },
+                  ),
+                )
+              ),
+              dotSize: 4,
+              dotSpacing: 15,
+              dotBgColor: Theme.of(context).primaryColor,
+              dotColor: Colors.white,
+              autoplay: false, // AutoPlay
             ),
-            dotSize: 4,
-            dotSpacing: 15,
-            dotBgColor: Theme.of(context).primaryColor,
-            dotColor: Colors.white,
-            autoplay: false, // AutoPlay
           ),
+            if(state.hasError)
+              Container(
+                alignment: Alignment.centerLeft,
+                margin: const EdgeInsets.only(top: 16,left: 16),
+                child: Text(
+                  state.errorText,
+                  style: const TextStyle(
+                    color: Colors.red,
+                    fontSize: 14
+                  ),
+                ),
+              ),
+        ]
         );
       }
     );
--- lib/screns/edit_product/edit_product_screen.dart
+++ lib/screns/edit_product/edit_product_screen.dart
@@ -5,22 +5,117 @@ import 'components/images_form.dart';
 
 class EditProductScreen extends StatelessWidget {
   final Product product;
+
+
+
   EditProductScreen(this.product);
+
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
+
+    final primaryCollor = Theme.of(context).primaryColor;
+
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text(
           'Editar Anúncio',
         ),
         centerTitle: true,
       ),
-      body: ListView(
-        children: [
-          ImagesForm(product),
-          TextFormField(
-          )
-        ],
+      body: Form(
+        key: formKey,
+        child: ListView(
+          children: [
+            ImagesForm(product),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  TextFormField(
+                    initialValue: product.name,
+                    decoration: const InputDecoration(
+                      hintText: "Título",
+                      border: InputBorder.none,
+                    ),
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600
+                    ),
+                    validator: (name){
+                      if(name.length<6){
+                        return 'Muito Curto';
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 4,bottom: 4),
+                    child: Text(
+                      'A partir de',
+                      style: TextStyle(
+                        color: Colors.grey[400],
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'R\$ ${product.basePrice.toStringAsFixed(2)}',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w800,
+                        color: Theme.of(context).primaryColor
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16,bottom: 8),
+                    child: Text(
+                      'Descrição',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  TextFormField(
+                    initialValue: product.description,
+                    style: const TextStyle(
+                      fontSize: 16,
+                    ),
+                    decoration: const InputDecoration(
+                      hintText: "Descrição",
+                      border: InputBorder.none,
+                    ),
+                    maxLines: null,
+                    validator: (desc){
+                      if(desc.length<10){
+                        return "Descrição muito Curta";
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                  RaisedButton(
+                    onPressed: (){
+                      if(formKey.currentState.validate()){
+                        print("Valido");
+                      } else {
+                        print("Invalido");
+                      }
+
+                    },
+                    child: const Text("Salvar"),
+                  ),
+                ],
+              ),
+            ),
+            TextFormField(
+            )
+          ],
+        ),
       ),
     );
   }
