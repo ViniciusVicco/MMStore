87a7bd9e2b9a5565d2fc934cb0bc55786272f500
VINICIUS VICCO PETRARCA DE ASSIS MIRANDA <vinicius.miranda@tre-to.jus.br>
2020-11-30 10:25:04 -0300

    Implementando o envio dos produtos e lógica pós compra.

---
+++ lib/screns/checkout/checkoutScreen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:michellemirandastore/common/price_card.dart';
+import 'package:michellemirandastore/models/cart_manager.dart';
+import 'package:michellemirandastore/models/checkout_manager.dart';
+import 'package:provider/provider.dart';
+class CheckoutScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProxyProvider<CartManager, CheckoutManager>(
+      create: (_) => CheckoutManager(),
+      update: (_, cartManager, checkoutManager) => checkoutManager..updateCart(cartManager),
+      lazy: false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Pagamento"),
+          centerTitle: true,
+        ),
+        body: ListView(
+          children: [
+            PriceCard(
+              buttonText: 'Finalizar Pedido',
+              onPressed: () {
+                Navigator.of(context).pushNamed('/checkout');
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
