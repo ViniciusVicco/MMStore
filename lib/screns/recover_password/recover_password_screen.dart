91c9ae6ce88ef6c6dcb8ffe4a5fbde6bbf298acb
viniciusvicco <viniciusvicco@gmail.com>
2021-05-04 15:53:17 -0300

    Altera titulo e aplica teste na tela de recuperação de senha

--- lib/screns/recover_password/recover_password_screen.dart
+++ lib/screns/recover_password/recover_password_screen.dart
@@ -8,6 +8,8 @@ class RecoverPassowrd extends StatefulWidget {
 class _RecoverPassowrdState extends State<RecoverPassowrd> {
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final heigth = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
         title: const Text("Recuperação de senha"),
@@ -16,42 +18,47 @@ class _RecoverPassowrdState extends State<RecoverPassowrd> {
         child: Container(
           width: MediaQuery.of(context).size.width * 0.9,
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: Colors.white
-          ),
+              borderRadius: BorderRadius.circular(10), color: Colors.white),
           child: ListView(
             shrinkWrap: true,
             children: [
               SizedBox(
-                   height:  MediaQuery.of(context).size.height * 0.5,
-                   child: Column(
-                     children: [
-                       SizedBox(
-                         height: 20,
-                       ),
-                       Container(
-                             width: MediaQuery.of(context).size.width * 0.8,
-                         decoration: BoxDecoration(
-                           borderRadius: BorderRadius.circular(10),
-                           border: Border.all(
-                             color: Theme.of(context).scaffoldBackgroundColor,
-
-                           )
-                         ),
-                         child: TextFormField(
-                           decoration: InputDecoration(
-                             border: InputBorder.none
-                           ),
-                         ),
-                       )
-                     ],
-                   ),
+                height: heigth * 0.03,
+              ),
+              Container(
+                  alignment: Alignment.center,
+                  child: Text(
+                    "Digite o seu e-mail para recuperar senha",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                    ),
+                  )),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.5,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(
+                            color: Theme.of(context).scaffoldBackgroundColor,
+                          )),
+                      child: TextFormField(
+                        decoration: InputDecoration(border: InputBorder.none),
+                      ),
+                    )
+                  ],
+                ),
               )
             ],
           ),
         ),
       ),
-      
     );
   }
 }
